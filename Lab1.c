5fd1e5a0d8f5cefcca7fa1531c2a3f0b808507e9
Lector07 <148982105+Lector07@users.noreply.github.com>
2023-11-15 15:12:06 +0100

    Lab1.c

--- Lab1.c
+++ Lab1.c
@@ -1,50 +1,99 @@
 #include <stdio.h>
 
 int main() {
-    int intA, a, b;
+	/*int numer_albumu;
 
-    printf("-=-=-=-=KALKULATOR=-=-=-=-\n");
-    printf("Wybierz dzialanie:\n");
-    printf("1. Suma\n");
-    printf("2. Roznica\n");
-    printf("3. Iloczyn\n");
-    printf("4. Iloraz\n");
-    printf("5. Zamknij\n\n");
-    scanf_s("%d", &intA);
+	printf("Podaj numer albumu: ");
 
-    printf("Podaj pierwsza liczbe: ");
-    scanf_s("%d", &a);
+	scanf("%d", &numer_albumu);
 
-    printf("Podaj druga liczbe: ");
-    scanf_s("%d", &b);
+	printf("Numer albumu urzytkownika : %d", numer_albumu);*/
 
-    switch (intA) {
-    case 1:
-        printf("%d\n", a + b);
-        break;
-    case 2:
-        printf("%d\n", a - b);
-        break;
-    case 3:
-        printf("%d\n", a * b);
-        break;
-    case 4:
-        if (b != 0) {
-            printf("%d\n", a / b);
-        }
-        else {
-            printf("Liczba nie moze byc zerem!\n");
-        }
-        break;
-    case 5:
-        break;
-    default:
-        printf("Nie rozpoznano polecenia\n");
-        break;
-    }
 
-    return 0;
+	//int liczbaA;
+
+	//printf("Podaj liczbe: ");
+	//scanf("%d", &liczbaA);
+
+	//if (liczbaA %2 == 0) {
+	//	printf("Podana liczba jest parzysta!");
+	//}
+	//else
+	//{
+	//	printf("Podana liczba jest nieparzysta!");
+	//}
+
+	//float a, b;
+
+	//printf("Podaj liczby: ");
+	//scanf("%f", &a);
+	//printf(" ");
+	//scanf("%f", &b);
+
+	//if (a > b) {
+	//	printf("Pierwsza iczba jest wieksza!");
+	//}
+	//else
+	//{
+	//	printf("Druga iczba jest wieksza!");
+	//}
+
+	//float a, b, c, d;
+	//printf("Podaj liczby: ");
+	//scanf("%f", &a);
+	//printf("\n");
+	//scanf("%f", &b);
+	//printf("\n");
+	//scanf("%f", &c);
+	//printf("\n");
+	//scanf("%f", &d);
+
+	//float srednia;
+	//srednia = (a + b + c + d) / 4;
+
+	//printf("Srednia: %f", srednia);
+
+	//int intA, a, b;
+
+	//printf("-=-=-=-=KALKULATOR=-=-=-=-\n");
+	//printf("Wybierz dzialanie:\n");
+	//printf("1. Suma\n");
+	//printf("2. Roznica\n");
+	//printf("3. Iloczyn\n");
+	//printf("4. Iloraz\n");
+	//printf("5. Zamknij\n\n");
+	//scanf_s("%d", &intA);
+
+	//printf("Podaj pierwsza liczbe: ");
+	//scanf_s("%d", &a);
+
+	//printf("Podaj druga liczbe: ");
+	//scanf_s("%d", &b);
+
+	//switch (intA) {
+	//case 1:
+	//	printf("%d\n", a + b);
+	//	break;
+	//case 2:
+	//	printf("%d\n", a - b);
+	//	break;
+	//case 3:
+	//	printf("%d\n", a * b);
+	//	break;
+	//case 4:
+	//	if (b != 0) {
+	//		printf("%d\n", a / b);
+	//	}
+	//	else {
+	//		printf("Liczba nie moze byc zerem!\n");
+	//	}
+	//	break;
+	//case 5:
+	//	break;
+	//default:
+	//	printf("Nie rozpoznano polecenia\n");
+	//	break;
+	//}
+
+
 }
-
-
-
