42413a0ff328b0ed782fd5d597e4a898536a3685
Lector07 <148982105+Lector07@users.noreply.github.com>
2023-12-02 13:15:40 +0100

    Add files via upload

---
+++ Lab02.c
@@ -0,0 +1,165 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <Windows.h>
+
+int main() {
+    //int liczba = 0;      //zad1
+    //int ile_liczb = 0;
+    //int suma = 0;
+    //float srednia = 0;
+    //int max = 0;
+
+    //while (liczba >= 0) {
+    //    printf("Podaj liczbe: ");
+    //    scanf_s("%d", &liczba);
+
+    //    if (liczba < 0) break;
+
+    //    ile_liczb++;
+    //    suma = suma + liczba;
+    //    srednia = (float)suma / ile_liczb;
+
+    //    if (liczba > max) {
+    //        max = liczba;
+    //    }
+    //}
+
+    //printf("Suma: %d\n", suma);
+    //printf("Srednia: %.2f\n", srednia);
+    //printf("Wartosc max: %d", max);
+
+
+    //int ile_ujemnych = 0;   //zad2
+    //int ile_dodatnich = 0;
+    //int suma_ujemnych = 0;
+    //int suma_dodatnich = 0;
+    //int liczba = 0;
+
+    //for (int i = 0; i < 10; i++) {
+    //    printf("Podaj liczbe: ");
+    //    scanf_s("%d", &liczba);
+
+    //    if (liczba > 0) {
+    //        ile_dodatnich++;
+    //        suma_dodatnich = suma_dodatnich + liczba;
+    //    }
+
+    //    if (liczba < 0) {
+    //        ile_ujemnych++;
+    //        suma_ujemnych = suma_ujemnych + liczba;
+    //    }
+    //}
+    //printf("Suma ujemnych: %d\n", suma_ujemnych);
+    //printf("Suma dodatnich: %d\n", suma_dodatnich);
+    //printf("Liczba ujemnych: %d\n", ile_ujemnych);
+    //printf("Liczba dodatnich: %d\n", ile_dodatnich);
+
+
+    //int n = 0;    //zad3
+    //int suma_parzystych = 0;
+
+    //printf("Podaj najwieksza wartosc ciagu: ");
+    //scanf_s("%d", &n);
+
+    //for (int i = 1; i <= n; i++) {
+    //    if (i % 2 == 0) {
+    //        suma_parzystych = suma_parzystych + i;
+    //    }
+
+    //}
+    //printf("Suma parzystych wynosi: %d", suma_parzystych);
+
+
+    //int ile_liczb = 0;    //zad4
+    //int liczba = 0;
+    //int suma_parzystych = 0;
+    //srand(time(NULL));
+
+    //printf("Podaj ilosc liczb: ");
+    //scanf_s("%d", &ile_liczb);
+
+    //for (int i = 0; i < ile_liczb; i++) {
+    //    liczba = rand() % 55 - 10;
+    //    if (liczba % 2 == 0) {
+    //        suma_parzystych = suma_parzystych + liczba;
+    //    }
+    //    printf("liczba: %d\n", liczba);
+    //}
+    //printf("Suma liczb parzystych z przedzialu (-10:45) to %d", suma_parzystych);
+
+    //for (int i = 1; i < 101; i++) {     //zad6
+    //    printf("%d, ", i);
+    //}
+
+    //printf("\n");
+    //printf("\n");
+    //printf("\n");
+
+    //for (int i = 100; i >=0; i--) {
+    //    printf("%d, ", i);
+    //}
+
+    //printf("\n");
+    //printf("\n");
+    //printf("\n");
+
+    //for (int i = 7; i <= 77; i+=7) {
+    //    printf("%d, ", i);
+    //}
+
+    //printf("\n");
+    //printf("\n");
+    //printf("\n");
+
+    //for (int i = 20; i >= 0; i -= 2) {
+    //    printf("%d, ", i);
+    //}
+
+    //printf("\n");
+
+
+    //int a, b;     //zad7
+    //for (a = 10; a >= 0; a--) {
+    //    for (b = 0; b <= a; b++)
+    //        printf("X");
+    //    printf("\n");
+    //   }
+    //
+    //printf("\n");
+    //printf("\n");
+
+    //int i, j;
+    //
+    //for (i = 0; i <= 10; i++) {
+    //    for (j = 0; j < 10 - i; j++)
+    //        printf(" ");
+    //
+    //    for (j = 0; j < i; j++)
+    //        printf("X");
+    //
+    //    printf("\n");
+    //}
+
+    //printf("\n");
+    //printf("\n");
+
+
+    //int x, y, k;
+
+    //for (x = 0; x <= 10; x++) {
+    //    for (y = 0; y < 10 - x; y++)
+    //        printf(" ");
+
+    //    for (k = 0; k < x; k++)
+    //        printf("X");
+
+    //    for (k = 0; k < x - 1; k++)
+    //        printf("X");
+
+    //    printf("\n");
+    //}
+
+
+    return 0;
+}
